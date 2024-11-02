f0ad4d6001afda7aa3b0f77bc542b19ca66e53a9
itmeJanHelbling <zolo_2010@hotmail.ch>
2024-11-02 09:03:49 +0100

    Grundgerüst von Java Springboot erstellt. Mysql Datenbank als Docker Container erstellt.

---
+++ lb_projekt/src/main/java/ch/wiss/m295/lb_projekt/LbProjektApplication.java
@@ -0,0 +1,13 @@
+package ch.wiss.m295.lb_projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LbProjektApplication.class, args);
+	}
+
+}
---
+++ lb_projekt/src/test/java/ch/wiss/m295/lb_projekt/LbProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.wiss.m295.lb_projekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
